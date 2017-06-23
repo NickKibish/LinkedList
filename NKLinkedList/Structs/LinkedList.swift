1f0461f6dabd70a0cbfaca0427af1ed0be8433c7
Mykola Kibysh <mykola.kibysh@ticketmaster.com>
2017-06-23 16:23:26 +0300

    added all necessary methods and propertios

--- NKLinkedList/Structs/LinkedList.swift
+++ NKLinkedList/Structs/LinkedList.swift
@@ -8,17 +8,79 @@
 import Foundation
 
 public struct LinkedList<T> {
+    public struct LinkedListIterator: IteratorProtocol {
+        var node: Node?
+        mutating public func next() -> Node? {
+            return node?.next
+        }
+    }
     
-    
-    fileprivate struct Node {
-        var val: T
+    public class Node {
+        var value: T
+        var next: Node?
+        weak var previous: Node?
         
+        init(value: T) {
+            self.value = value
+        }
     }
     
     fileprivate var root: Node?
 }
-/*
-extension LinkedList: Sequence {
+
+// MARK: - Public Properties
+extension LinkedList {
+    public var isEmpty: Bool {
+        return root == nil
+    }
     
+    public var head: Node? {
+        return root
+    }
+    
+    public var tail: Node? {
+        if var node = root {
+            while case let next? = node.next {
+                node = next
+            }
+            return node
+        }
+        return nil
+    }
+    
+    public var count: Int {
+        if var node = root {
+            var c = 1
+            while case let next? = node.next {
+                node = next
+                c += 1
+            }
+            return c
+        }
+        return 0
+    }
 }
-*/
+
+// MARK: - Public Methods
+extension LinkedList {
+    mutating public func append(value: T) {
+        let newNode = Node(value: value)
+        if let lastNode = tail {
+            newNode.previous = lastNode
+            lastNode.next = newNode
+        } else {
+            root = newNode
+        }
+    }
+    
+    mutating public func removeAll() {
+        root = nil
+    }
+}
+
+extension LinkedList: Sequence {
+    public func makeIterator() -> LinkedListIterator {
+        return LinkedListIterator(node: root)
+    }
+}
+
